8dbfdedd91139e30cc1647b29528c6d368c88d54
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2023-03-14 15:10:18 +0100

    feat: API request - error handling

--- src/API-request.ts
+++ src/API-request.ts
@@ -4,18 +4,26 @@ export const getAllCountries = async (
   API_URL: string,
   countries: requiredDataType[]
 ) => {
-  const response: Response = await fetch(API_URL);
-  const data: Array<object> = await response.json();
+  try {
+    const response: Response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    } else {
+      const data: Array<object> = await response.json();
 
-  countries = data.map((country: responseDataType) => {
-    return {
-      capital: country.capital && country.capital[0],
-      population: country.population,
-      name: country.name.common,
-      region: country.region,
-      flagUrl: country.flags.png,
-    };
-  });
+      countries = data.map((country: responseDataType) => {
+        return {
+          capital: country.capital && country.capital[0],
+          population: country.population,
+          name: country.name.common,
+          region: country.region,
+          flagUrl: country.flags.png,
+        };
+      });
+    }
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
 
   return countries;
 };
